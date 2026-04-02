438c501cea9b998612ccb8ba3e0fc2cd31485456
Astawus <yanosastawus@gmail.com>
2026-04-02 19:29:48 +0300

    backend port made dynamic

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const db = require('./database');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
